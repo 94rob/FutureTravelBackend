cf83f29b50e85a53758e9aa845d9ef9f87f41fc2
icts21-23.170 <fabrizio.bruno@edu.itspiemonte.it>
2022-12-28 17:01:20 +0100

    Test

---
+++ src/test/java/its/extratech/FutureTravel/controllerTests/FEControllerTests/ResidenzaClienteAggragateTest.java
@@ -0,0 +1,10 @@
+package its.extratech.FutureTravel.controllerTests.FEControllerTests;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ResidenzaClienteAggragateTest {
+}
