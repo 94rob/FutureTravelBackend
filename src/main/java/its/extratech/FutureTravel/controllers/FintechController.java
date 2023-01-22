0b15cff33a8af9fccebfd34095db0aed215c93cf
BrunoFabrizio <fabriziobrunop@gmail.com>
2023-01-22 16:37:34 +0100

    Prova test Post

--- src/main/java/its/extratech/FutureTravel/controllers/FintechController.java
+++ src/main/java/its/extratech/FutureTravel/controllers/FintechController.java
@@ -2,24 +2,46 @@ package its.extratech.FutureTravel.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import its.extratech.FutureTravel.dtos.request.FintechRequest;
 import its.extratech.FutureTravel.dtos.response.RecordDtoPresenze;
-import its.extratech.FutureTravel.servicies.implementations.RecordServiceImpl;
+import its.extratech.FutureTravel.entities.Record;
+import its.extratech.FutureTravel.servicies.implementations.*;
+import its.extratech.FutureTravel.entities.*;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/fintech")
 public class FintechController {
 
     @Autowired
+    TipoAlloggioServiceImpl tipoAlloggioService;
+
+    @Autowired
+    ProvinciaServiceImpl provinciaService;
+
+    @Autowired
+    ResidenzaClientiServiceImpl residenzaClientiService;
+
+    @Autowired
+    ContestoServiceImpl contestoService;
+
+
+    @Autowired
     private RecordServiceImpl recordService;
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    FintechServiceImpl fintechService;
 
     @GetMapping("/all")
     public ResponseEntity<?> getAll() throws JsonProcessingException {
@@ -63,6 +85,12 @@ public class FintechController {
         return new ResponseEntity<>(this.objectMapper.writeValueAsString(recordDtoPresenzeList), HttpStatus.OK);
     }
 
+    @PostMapping("/upload")
+    public ResponseEntity<?>  UploadJson(@Valid @RequestBody List<FintechRequest> newRecord) throws JsonProcessingException {
 
+        //fintechService.saveUpload(newRecord);
+        fintechService.saveListUpload(newRecord);
+        return new ResponseEntity<>(newRecord.toString(), HttpStatus.OK);
+    }
 
 }
--- src/main/java/its/extratech/FutureTravel/repositories/ResidenzaClientiRepository.java
+++ src/main/java/its/extratech/FutureTravel/repositories/ResidenzaClientiRepository.java
@@ -1,5 +1,6 @@
 package its.extratech.FutureTravel.repositories;
 
+import its.extratech.FutureTravel.entities.Provincia;
 import its.extratech.FutureTravel.entities.ResidenzaClienti;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ResidenzaClientiRepository extends JpaRepository<ResidenzaClienti, String> {
 
+    ResidenzaClienti findByDescrizione(String descrizione);
 
 }
